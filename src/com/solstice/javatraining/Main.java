3b87900a81daeb19c54b9c3e5dbcadea908db100
Tyree Solomon-Phillips <tyreesolomon-phillips@Tyree-Solomon-Phillips-MacBook-Pro.local>
2018-07-03 00:27:13 -0500

    uncommented main class

--- src/com/solstice/javatraining/Main.java
+++ src/com/solstice/javatraining/Main.java
@@ -17,32 +17,32 @@ public class Main
 
         // will parse the json file and insert it into your database
 
-//        File jsonFile = new File("week1-stocks.json").getAbsoluteFile();
-//        ObjectMapper objectMapper = new ObjectMapper();
-//
-//        try
-//        {
-//            List<Stock> stockList = objectMapper.readValue(jsonFile, new TypeReference<List<Stock>>(){});
-//            Stock stock = new Stock();
-//
-//            for (int i = 0; i < stockList.size(); i++)
-//            {
-//                stock.setSymbol(stockList.get(i).getSymbol());
-//                stock.setPrice(stockList.get(i).getPrice());
-//                stock.setVolume(stockList.get(i).getVolume());
-//                stock.setDate(stockList.get(i).getDate());
-//
-//                StockManager.insert(stock);
-//            }
-//
-//            System.out.println("Data has been successfully inserted");
-//
-//        }
-//        catch (IOException e)
-//        {
-//            e.printStackTrace();
-//            e.getMessage();
-//        }
+        File jsonFile = new File("week1-stocks.json").getAbsoluteFile();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try
+        {
+            List<Stock> stockList = objectMapper.readValue(jsonFile, new TypeReference<List<Stock>>(){});
+            Stock stock = new Stock();
+
+            for (int i = 0; i < stockList.size(); i++)
+            {
+                stock.setSymbol(stockList.get(i).getSymbol());
+                stock.setPrice(stockList.get(i).getPrice());
+                stock.setVolume(stockList.get(i).getVolume());
+                stock.setDate(stockList.get(i).getDate());
+
+                StockManager.insert(stock);
+            }
+
+            System.out.println("Data has been successfully inserted");
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            e.getMessage();
+        }
 
         // Collecting user input
 
